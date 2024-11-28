73ca4bd9ca724ef5088e76388a979505f6cd60fc
Ricardo Costa <rcosta.ms358@gmail.com>
2024-11-28 23:06:40 +0000

    Update Language Dictionary

--- code/app/src/main/java/fcul/cmov/voidnetwork/ui/viewmodels/LanguageViewModel.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/ui/viewmodels/LanguageViewModel.kt
@@ -1,20 +1,24 @@
 package fcul.cmov.voidnetwork.ui.viewmodels
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import fcul.cmov.voidnetwork.domain.Language
 
+const val MAX_MESSAGE_LENGTH = 20
+const val MAX_CODE_LENGTH = 20
+
 class LanguageViewModel : ViewModel() {
     // handles language crud operations and selection
 
     val languages by mutableStateOf(
-        mutableMapOf(
+        mutableStateMapOf(
             "abc" to Language(
                 id = "abc",
                 name = "Morse Code",
-                dictionary = mapOf(
+                dictionary = mutableStateMapOf(
                     "...___..." to "SOS",
                     ".-.. --- ...- ." to "LOVE",
                     ".... . .-.. .-.. ---" to "HELLO",
@@ -28,7 +32,7 @@ class LanguageViewModel : ViewModel() {
             "def" to Language(
                 id = "def",
                 name = "Simple Lang",
-                dictionary = mapOf(
+                dictionary = mutableStateMapOf(
                     "..--.." to "HELLO",
                     "--..--" to "GOODBYE",
                     ".-.--." to "FRIEND",
@@ -45,12 +49,19 @@ class LanguageViewModel : ViewModel() {
         return language ?: Language(
             id = id,
             name = "New Language",
-            dictionary = mapOf()
+            dictionary = mutableMapOf()
         ).also { languages[id] = it }
     }
 
     fun selectLanguage(id: String) {
         require(languages.containsKey(id)) { "Language with id $id does not exist" }
         languageSelected = languages[id]
+    }
+
+    fun onUpdateLanguageDictionary(languageId: String, code: String, msg: String) {
+        require(code.length <= MAX_CODE_LENGTH) { "Code length must be less than $MAX_CODE_LENGTH" }
+        require(msg.length <= MAX_MESSAGE_LENGTH) { "Message length must be less than $MAX_MESSAGE_LENGTH" }
+        val language = getOrAddLanguageById(languageId)
+        language.dictionary[code] = msg
     }
 }
