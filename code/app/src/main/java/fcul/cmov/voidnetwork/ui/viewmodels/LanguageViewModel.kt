d45b6004d9f051ccf9dffd29389ff8a3039348de
Ricardo Costa <rcosta.ms358@gmail.com>
2024-12-12 19:37:13 +0000

    Implemented LanguageViewModel with Firebase

--- code/app/src/main/java/fcul/cmov/voidnetwork/ui/viewmodels/LanguageViewModel.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/ui/viewmodels/LanguageViewModel.kt
@@ -1,56 +1,46 @@
 package fcul.cmov.voidnetwork.ui.viewmodels
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import fcul.cmov.voidnetwork.domain.Language
-
-const val MAX_MESSAGE_LENGTH = 20
-const val MAX_CODE_LENGTH = 20
 
 class LanguageViewModel : ViewModel() {
     // handles language crud operations and selection
 
-    val languages by mutableStateOf(
-        mutableStateMapOf(
-            "abc" to Language(
-                id = "abc",
-                name = "Morse Code",
-                dictionary = mutableStateMapOf(
-                    "...___..." to "SOS",
-                    ".-.. --- ...- ." to "LOVE",
-                    ".... . .-.. .-.. ---" to "HELLO",
-                    "--. --- --- -.. -... -.-- ." to "GOODBYE",
-                    "..-. .-. .. . -. -.." to "FRIEND",
-                    "- .... .- -. -.-" to "THANK YOU",
-                    "..-. ..- -." to "FUN",
-                    "-.-. --- --- .-." to "COOL",
-                )
-            ),
-            "def" to Language(
-                id = "def",
-                name = "Simple Lang",
-                dictionary = mutableStateMapOf(
-                    "..--.." to "HELLO",
-                    "--..--" to "GOODBYE",
-                    ".-.--." to "FRIEND",
-                    "--.-.." to "THANK YOU"
-                )
-            )
-        )
-    )
+    private val database by lazy { Firebase.database.reference }
 
-    var languageSelected: Language? by mutableStateOf(languages["abc"])
+    private val languageDatabase
+        get() = database.child("languages")
 
-    fun getOrAddLanguageById(id: String): Language {
-        val language = languages[id]
-        return language ?: Language(
-            id = id,
-            name = "New Language",
-            dictionary = mutableMapOf()
-        ).also { languages[id] = it }
+
+    val languages = mutableStateMapOf<String, Language>()
+    var languageSelected: Language? by mutableStateOf(null)
+
+    init {
+        loadLanguagesFromFirebase()
+        listenToChangesFromFirebase()
+    }
+
+    companion object {
+        const val MAX_MESSAGE_LENGTH = 20
+        const val MAX_CODE_LENGTH = 20
+    }
+
+    fun getLanguage(id: String): Language {
+        return languages[id] ?: throw IllegalStateException("Language with id $id does not exist")
+    }
+
+    fun addLanguage(onCompleted: (String) -> Unit) {
+        addLanguageToFirebase(onCompleted)
     }
 
     fun selectLanguage(id: String) {
@@ -58,10 +48,94 @@ class LanguageViewModel : ViewModel() {
         languageSelected = languages[id]
     }
 
-    fun onUpdateLanguageDictionary(languageId: String, code: String, msg: String) {
+    fun deleteLanguage(id: String) {
+        deleteLanguageFromFirebase(id)
+    }
+
+    fun onUpdateLanguageDictionary(languageId: String, code: String, msg: String) {7
         require(code.length <= MAX_CODE_LENGTH) { "Code length must be less than $MAX_CODE_LENGTH" }
         require(msg.length <= MAX_MESSAGE_LENGTH) { "Message length must be less than $MAX_MESSAGE_LENGTH" }
-        val language = getOrAddLanguageById(languageId)
-        language.dictionary[code] = msg
+
+        val language = getLanguage(languageId)
+        val updatedLanguage = language.copy(dictionary = language.dictionary + (code to msg))
+        updateLanguageInFirebase(updatedLanguage)
+    }
+
+    fun onDeleteMessageFromLanguage(id: String, code: String) {
+        val language = getLanguage(id)
+        val updatedLanguage = language.copy(dictionary = language.dictionary - code)
+        updateLanguageInFirebase(updatedLanguage)
+    }
+
+    private fun loadLanguagesFromFirebase() {
+        languageDatabase.get().addOnSuccessListener { dataSnapshot ->
+            // get languages from database
+            val languages = dataSnapshot.children.associate { snapshot ->
+                val language = snapshot.getValue(Language::class.java)
+                requireNotNull(language) { "Language is null" }
+                language.id to language
+            }
+            // update languages in memory
+            synchronized(languages) {
+                this.languages.clear()
+                this.languages.putAll(languages)
+            }
+        }
+    }
+
+    private fun addLanguageToFirebase(onCompleted: (String) -> Unit): String {
+        val newRef = languageDatabase.push()
+        val newId = newRef.key ?: throw IllegalStateException("Failed to generate a new key for the language")
+        val newLanguages = languages.values.count { it.name.startsWith("New Language") }
+        val name = "New Language${if (newLanguages > 0) " ($newLanguages)" else ""}"
+        val newLanguage = Language(newId, name, emptyMap())
+        newRef.setValue(newLanguage)
+            .addOnSuccessListener {
+                onCompleted(newId)
+            }
+        return newId
+    }
+
+    private fun updateLanguageInFirebase(language: Language) {
+        languageDatabase.child(language.id).setValue(language)
+    }
+
+    private fun deleteLanguageFromFirebase(id: String) {
+        languageDatabase.child(id).removeValue()
+    }
+
+    private fun listenToChangesFromFirebase() {
+        // updates languages in memory when database changes
+        languageDatabase.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val language = snapshot.getValue(Language::class.java)
+                requireNotNull(language) { "Language is null" }
+                synchronized(languages) {
+                    languages[language.id] = language
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val language = snapshot.getValue(Language::class.java)
+                requireNotNull(language) { "Language is null" }
+                synchronized(languages) {
+                    languages[language.id] = language
+                }
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                val language = snapshot.getValue(Language::class.java)
+                requireNotNull(language) { "Language is null" }
+                synchronized(languages) {
+                    languages.remove(language.id)
+                }
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("LanguageViewModel", "Database error: $error")
+            }
+        })
     }
 }
