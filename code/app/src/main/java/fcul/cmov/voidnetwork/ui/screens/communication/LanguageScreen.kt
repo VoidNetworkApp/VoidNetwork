a059ec6aa188913793c2b9293120462bf8db0a35
Ricardo Costa <rcosta.ms358@gmail.com>
2024-12-14 16:06:51 +0000

    Bug Fixes & Other Changes

--- code/app/src/main/java/fcul/cmov/voidnetwork/ui/screens/communication/LanguageScreen.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/ui/screens/communication/LanguageScreen.kt
@@ -4,38 +4,47 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import fcul.cmov.voidnetwork.R
 import fcul.cmov.voidnetwork.domain.Language
+import fcul.cmov.voidnetwork.ui.utils.MAX_MESSAGE_LENGTH
 import fcul.cmov.voidnetwork.ui.utils.ScreenWithTopBar
 import fcul.cmov.voidnetwork.ui.utils.rememberPressSequence
 import fcul.cmov.voidnetwork.ui.viewmodels.LanguageViewModel
-import fcul.cmov.voidnetwork.ui.viewmodels.LanguageViewModel.Companion.MAX_MESSAGE_LENGTH
 
 @Composable
 fun LanguageScreen(
@@ -43,52 +52,67 @@ fun LanguageScreen(
     viewModel: LanguageViewModel,
     id: String
 ) {
+    val language = viewModel.getLanguageOrNull(id)
+    if (language == null) {
+        // navigate back if the language is deleted or not found
+        LaunchedEffect(Unit) {
+            nav.popBackStack()
+        }
+        return
+    }
     ScreenWithTopBar(
         title = stringResource(R.string.language_dictionary),
         nav = nav
     ) { paddingValues ->
         LanguageScreenContent(
-            nav = nav,
             modifier = Modifier.padding(paddingValues),
             language = viewModel.getLanguage(id),
             onDeleteMessage = { code -> viewModel.onDeleteMessageFromLanguage(id, code) },
             onUpdateLanguageDictionary = { code, msg ->
                 viewModel.onUpdateLanguageDictionary(id, code, msg)
-            }
+            },
+            onDeleteLanguage = { viewModel.deleteLanguage(id) },
+            onEditLanguage = { viewModel.onEditLanguage(language.id, it) }
         )
     }
 }
 
 @Composable
 fun LanguageScreenContent(
-    nav: NavController,
     language: Language,
     onUpdateLanguageDictionary: (String, String) -> Unit,
     onDeleteMessage: (String) -> Unit,
+    onDeleteLanguage: () -> Unit,
+    onEditLanguage: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     var isAddingMessage by rememberSaveable { mutableStateOf(false) }
     Column(
-        modifier = modifier,
+        modifier = modifier.fillMaxHeight().verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        LanguageDictionary(
+        LanguageTopView(
             language = language,
-            modifier = modifier,
-            onDeleteMessage = onDeleteMessage,
+            onDeleteLanguage = onDeleteLanguage,
+            onEditLanguage = onEditLanguage,
+            modifier = Modifier.fillMaxWidth().padding(20.dp)
         )
+        Box(
+            modifier = Modifier.fillMaxWidth().weight(1f)
+        ) {
+            LanguageDictionary(
+                language = language,
+                onDeleteMessage = onDeleteMessage,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.padding(20.dp)
         ) {
-            Button(onClick = { isAddingMessage = !isAddingMessage }) {
-                Text(
-                    text = if (isAddingMessage) stringResource(R.string.cancel)
-                        else stringResource(R.string.add_message)
-                )
-            }
             if (isAddingMessage) {
                 AddMessageForm(
                     onUpdateLanguageDictionary = { code, msg ->
@@ -97,6 +121,89 @@ fun LanguageScreenContent(
                     }
                 )
             }
+            Button(onClick = { isAddingMessage = !isAddingMessage }) {
+                Text(
+                    text = if (isAddingMessage) stringResource(R.string.cancel)
+                    else stringResource(R.string.add_message)
+                )
+            }
+        }
+    }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LanguageTopView(
+    language: Language,
+    onDeleteLanguage: () -> Unit,
+    onEditLanguage: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var isEditing by rememberSaveable { mutableStateOf(false) }
+    var languageName by rememberSaveable { mutableStateOf(language.name) }
+
+    // in case of external changes, keep language name in sync
+    LaunchedEffect(language.name) {
+        if (!isEditing) languageName = language.name
+    }
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        if (isEditing) {
+            TextField(
+                value = languageName,
+                onValueChange = { languageName = it },
+                label = { Text(stringResource(R.string.language_name)) },
+                modifier = Modifier.weight(1f)
+            )
+            Button(
+                onClick = {
+                    onEditLanguage(languageName)
+                    isEditing = false
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(R.string.save_changes),
+                )
+            }
+        } else {
+            Text(
+                text = language.name,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(vertical = 30.dp).weight(1f)
+            )
+            Button(
+                onClick = { isEditing = true },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            ) {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = stringResource(R.string.edit_language)
+                )
+            }
+        }
+        Button(
+            onClick = onDeleteLanguage,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.background,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Icon(
+                Icons.Filled.Delete,
+                contentDescription = stringResource(R.string.delete_language),
+                modifier = Modifier.size(24.dp)
+            )
         }
     }
 }
@@ -135,15 +242,14 @@ fun AddMessageForm(
         }
         Text(
             text = sequence,
-            fontSize = 40.sp
+            fontSize = 40.sp,
+            modifier = Modifier.padding(0.dp)
         )
         Button(
             onClick = {
-                if (sequence.isNotBlank() && message.isNotBlank()) {
-                    onUpdateLanguageDictionary(sequence, message)
-                    resetSequence()
-                    message = ""
-                }
+                onUpdateLanguageDictionary(sequence, message)
+                resetSequence()
+                message = ""
             },
             enabled = sequence.isNotBlank() && message.isNotBlank()
         ) {
@@ -159,62 +265,75 @@ fun LanguageDictionary(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            text = language.name,
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Spacer(Modifier.size(50.dp))
-
-        // dictionary table
+        // dictionary table header
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.code),
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.weight(1f),
+            )
+            Text(
+                text = stringResource(R.string.message),
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.weight(2f),
+            )
+            Text(
+                text = stringResource(R.string.actions),
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.weight(0.5f),
+                color = Color.Transparent
+            )
+        }
+        // dictionary table rows
         LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            // table header row
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(R.string.code),
-                        modifier = Modifier.weight(1f),
-                    )
-                    Text(
-                        text = stringResource(R.string.message),
-                        modifier = Modifier.weight(1f),
-                    )
-                }
-            }
-            // table rows
             items(language.dictionary.toList()) { (key, value) ->
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                        .padding(vertical = 4.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
                         text = key,
+                        style = MaterialTheme.typography.bodyMedium,
                         modifier = Modifier.weight(1f),
                     )
                     Text(
                         text = value,
-                        modifier = Modifier.weight(1f),
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.weight(2f),
                     )
-                    Button(onClick = { onDeleteMessage(key) }) {
+                    Button(
+                        onClick = { onDeleteMessage(key) },
+                        contentPadding = PaddingValues(4.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.background,
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        ),
+                        modifier = Modifier.weight(0.5f)
+                    ) {
                         Icon(
-                            Icons.Filled.Remove,
-                            contentDescription = stringResource(R.string.delete_message)
+                            imageVector = Icons.Filled.Remove,
+                            contentDescription = stringResource(R.string.delete_message),
+                            modifier = Modifier.size(16.dp)
                         )
                     }
                 }
--- code/app/src/main/java/fcul/cmov/voidnetwork/ui/utils/Constants.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/ui/utils/Constants.kt
@@ -3,6 +3,8 @@ package fcul.cmov.voidnetwork.ui.utils
 // Signal Communication Constants
 const val SHORT = "-"
 const val LONG = "_"
+const val MAX_MESSAGE_LENGTH = 20
+const val MAX_LANGUAGE_NAME_LENGTH = 20
 const val MAX_CODE_LENGTH = 15
 const val MIN_PRESS_DURATION_SHORT_PRESS = 250L
 const val MAX_INACTIVITY_DURATION = 5000L
