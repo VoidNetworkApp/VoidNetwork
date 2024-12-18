8eac3e1f4bae3a971eea6d958920e719ab401fa4
mstcardoso <mst.cardoso94@gmail.com>
2024-12-18 16:15:00 +0000

    Functionalities related to the Map almost done

--- code/app/src/main/java/fcul/cmov/voidnetwork/domain/Portal.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/domain/Portal.kt
@@ -1,3 +1,10 @@
 package fcul.cmov.voidnetwork.domain
 
-data class Portal(val street: String, val distance: Float) // other props...
+data class Portal(
+    val street: String,
+    val lat: Double,
+    val lon: Double)
+{
+    // No-argument constructor is required for Firebase to deserialize data.
+    constructor() : this("", 0.0, 0.0)
+}
