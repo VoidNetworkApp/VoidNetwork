935c5e7a53bc9f822f23e0ccc4bbd1482debf66f
Diogoapedro <diogoapedro02@gmail.com>
2025-01-13 16:38:40 +0000

    Merge corrected, image appears

--- code/app/src/main/java/fcul/cmov/voidnetwork/ui/screens/portal/RegisterPortalScreen.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/ui/screens/portal/RegisterPortalScreen.kt
@@ -1,7 +1,6 @@
 package fcul.cmov.voidnetwork.ui.screens.portal
 
 import android.net.Uri
-import android.os.Environment
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -10,26 +9,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import fcul.cmov.voidnetwork.R
 import fcul.cmov.voidnetwork.domain.Coordinates
-import fcul.cmov.voidnetwork.ui.navigation.Screens
 import fcul.cmov.voidnetwork.ui.utils.composables.CameraButton
-import fcul.cmov.voidnetwork.ui.utils.composables.CameraPhoto
 import fcul.cmov.voidnetwork.ui.utils.composables.ScreenWithTopBar
+import fcul.cmov.voidnetwork.ui.utils.composables.createImageFile
 import fcul.cmov.voidnetwork.ui.viewmodels.PortalViewModel
+import java.util.Objects
 
 @Composable
 fun RegisterPortalScreen(
@@ -63,31 +68,14 @@ fun RegisterPortalScreen(
 @Composable
 fun RegisterPortalScreenContent(
     nav: NavController,
+    modifier: Modifier = Modifier,
     capturedImageUri: Uri?,
     onPhotoCaptured: (Uri) -> Unit,
     onRegisterPortal: () -> Unit,
-    modifier: Modifier = Modifier,
 ) {
     val context = LocalContext.current
     val file = context.createImageFile()
     Log.d("TAG_FilePath", file.absolutePath)
-    val uri = FileProvider.getUriForFile(
-        Objects.requireNonNull(context),
-        context.getPackageName() + ".provider", file
-    )
-    var capturedImageUri by remember {
-        mutableStateOf<Uri>(Uri.EMPTY)
-    }
-    val cameraLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.TakePicture()){
-            capturedImageUri = uri
-        }
-    val permissionLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) {
-        Log.d("TAG", "here")
-        cameraLauncher.launch(uri)
-    }
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -102,18 +90,23 @@ fun RegisterPortalScreenContent(
                 .size(300.dp)
                 .background(Color.Black)
         )
-        Button(onClick = { if (latitude != null && longitude != null) { marker(latitude, longitude) }
-            nav.navigate(Screens.Main.route)
-                         }, enabled = true) {
-
-        /* CameraPhoto(capturedImageUri)
-        Button(
-            enabled = true, // TODO: only when photo is verified to be a portal
-            onClick = onRegisterPortal,
-        ) {
+        // TODO Add coordinates logic
+        // TODO Add Verified portal logic
+        Button(onClick = { Log.d("","") }) {
             Text(stringResource(R.string.register_portal))
-        } */
-        Text(stringResource(R.string.waiting_for_photo_verification))
+        }
+//        Button(onClick = { if (latitude != null && longitude != null) { marker(latitude, longitude) }
+//            nav.navigate(Screens.Main.route)
+//                         }, enabled = true) {
+//
+//        /* CameraPhoto(capturedImageUri)
+//        Button(
+//            enabled = true, // TODO: only when photo is verified to be a portal
+//            onClick = onRegisterPortal,
+//        ) {*/
+//            Text(stringResource(R.string.register_portal))
+//        }
+//        Text(stringResource(R.string.waiting_for_photo_verification))
         CameraButton(
             onPhotoCaptured = { onPhotoCaptured(it) },
             text = stringResource(R.string.capture_photo),
