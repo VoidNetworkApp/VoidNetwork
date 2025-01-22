1b6218b476d6968f535aaedddbc8762e50d51b3b
Diogoapedro <diogoapedro02@gmail.com>
2025-01-22 17:48:27 +0000

    Image corrected in the show portal screen

--- code/app/src/main/java/fcul/cmov/voidnetwork/ui/screens/portal/PortalScreen.kt
+++ code/app/src/main/java/fcul/cmov/voidnetwork/ui/screens/portal/PortalScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -94,6 +95,7 @@ fun PortalScreenContent(
         AsyncImage(
             model = imageUrl,
             contentDescription = stringResource(R.string.portal_captured_with_camera),
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(300.dp)
                 .background(Color.Black)
